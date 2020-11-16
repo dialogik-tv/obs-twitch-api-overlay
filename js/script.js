2d597feedbf0aac3a9a2720ef70f54704efb1d48
Moritz <moritz@dialogik.tv>
2020-11-16 09:06:52 +0100

    Clean up JS code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 (function() {
+    // Twitch API Client ID and dialogikTV channel ID
     const clientId = 'j8h3bcpwhkekt4wcwd0p4rydxefff4';
     const channelId = '265345534';
-    // const channel = 'dialogikTV';
 
+    // Twitch.js
     const { api } = new TwitchJs({ clientId });
 
+    // Content elements
     const latestFollower = document.getElementById('latest-follower');
     const followerCount  = document.getElementById('follower-count');
 
-    // channels/<channel ID>/follows
-
-    // Get featured streams.
+    // Get channel follows
     api.get(`channels/${channelId}/follows`, { version: 'kraken' }).then(response => {
-        console.log('response', response);
         followerCount.innerText  = response.total;
         latestFollower.innerText = response.follows[0].user.displayName;
-        // Do stuff ...
     });
 })();
