85808b9d811e0d35937d2e5b296ccd5740e94a87
Moritz <moritz@dialogik.tv>
2020-11-17 07:02:12 +0100

    Fetch subs via Helix API

--- js/script.js
+++ js/script.js
@@ -10,9 +10,31 @@
     const latestFollower = document.getElementById('latest-follower');
     const followerCount  = document.getElementById('follower-count');
 
-    // Get channel follows
-    api.get(`channels/${channelId}/follows`, { version: 'kraken' }).then(response => {
-        followerCount.innerText  = response.total;
-        latestFollower.innerText = response.follows[0].user.displayName;
-    });
+    const latestSub = document.getElementById('latest-sub');
+    const subCount  = document.getElementById('sub-count');
+
+    // Check if user comes from twitch authentication
+    if(window.location.hash !== '') {
+        // Extract token
+        var hash = window.location.hash.split('#')[1].split('&')[0].split('=');
+        var token = null;
+        if(hash[0] == 'access_token') {
+            var token = hash[1];
+
+            // Fetch username for full chat authability
+            const { api } = new window.TwitchJs({ token, clientId });
+
+            // Get channel follows
+            api.get(`users/follows?to_id=${channelId}`).then(response => {
+                followerCount.innerText  = response.total;
+                latestFollower.innerText = response.data[0].fromName;
+
+                api.get(`subscriptions?broadcaster_id=${channelId}`).then(response => {
+                    console.log(response);
+                    subCount.innerText  = response.data.length - 1;
+                    latestSub.innerText = response.data[0].userName;
+                });
+            });
+        }
+    }
 })();
