92d8056387a6b91808580aa251c6ee8330c2c150
Moritz <moritz@dialogik.tv>
2020-11-19 06:34:18 +0100

    Continuously fetch Twitch API

--- js/script.js
+++ js/script.js
@@ -15,26 +15,39 @@
 
     // Check if user comes from twitch authentication
     if(window.location.hash !== '') {
+        // Remove twitch connect link
+        const twitchConnector = document.getElementById('twitch-connector');
+        twitchConnector.parentNode.removeChild(twitchConnector);;
+
         // Extract token
         var hash = window.location.hash.split('#')[1].split('&')[0].split('=');
         var token = null;
         if(hash[0] == 'access_token') {
             var token = hash[1];
 
+            // Remove token from URL
+            history.pushState("", document.title, window.location.pathname);
+
             // Fetch username for full chat authability
             const { api } = new window.TwitchJs({ token, clientId });
 
-            // Get channel follows
-            api.get(`users/follows?to_id=${channelId}`).then(response => {
-                followerCount.innerText  = response.total;
-                latestFollower.innerText = response.data[0].fromName;
-
-                api.get(`subscriptions?broadcaster_id=${channelId}`).then(response => {
-                    console.log(response);
-                    subCount.innerText  = response.data.length - 1;
-                    latestSub.innerText = response.data[0].userName;
-                });
-            });
+            fetchApi(channelId, api, followerCount, latestFollower, subCount, latestSub);
+            setInterval(function() {
+                fetchApi(channelId, api, followerCount, latestFollower, subCount, latestSub);
+            }, 6000);
         }
     }
 })();
+
+function fetchApi(channelId, api, followerCount, latestFollower, subCount, latestSub) {
+    // Get channel follows
+    api.get(`users/follows?to_id=${channelId}`).then(response => {
+        followerCount.innerText  = response.total;
+        latestFollower.innerText = response.data[0].fromName;
+        
+        api.get(`subscriptions?broadcaster_id=${channelId}`).then(response => {
+            subCount.innerText  = response.data.length - 1;
+            latestSub.innerText = response.data[0].userName;
+        });
+    });
+}
