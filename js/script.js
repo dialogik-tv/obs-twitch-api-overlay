0d0a4b915d4fe0d90022653878decccb1076971e
Moritz <moritz@dialogik.tv>
2020-11-23 08:05:22 +0100

    Add animated labels for content display

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+var data = { 
+    "Follower": null,
+    "Letzter Follower": null,
+    "Subscriber": null,
+    "Letzter Sub": null
+};
+var fetched = false;
+var i = 0;
+
 (function() {
     // Twitch API Client ID and dialogikTV channel ID
     const clientId = 'mn01zj33eftqrsn760lac2l7nwnrls';
@@ -6,18 +15,11 @@
     // Twitch.js
     const { api } = new TwitchJs({ clientId });
 
-    // Content elements
-    const latestFollower = document.getElementById('latest-follower');
-    const followerCount  = document.getElementById('follower-count');
-
-    const latestSub = document.getElementById('latest-sub');
-    const subCount  = document.getElementById('sub-count');
-
     // Check if user comes from twitch authentication
     if(window.location.hash !== '') {
         // Remove twitch connect link
         const twitchConnector = document.getElementById('twitch-connector');
-        twitchConnector.parentNode.removeChild(twitchConnector);;
+        twitchConnector.parentNode.removeChild(twitchConnector);
 
         // Extract token
         var hash = window.location.hash.split('#')[1].split('&')[0].split('=');
@@ -31,23 +33,47 @@
             // Fetch username for full chat authability
             const { api } = new window.TwitchJs({ token, clientId });
 
-            fetchApi(channelId, api, followerCount, latestFollower, subCount, latestSub);
+            fetchApi(channelId, api);
             setInterval(function() {
-                fetchApi(channelId, api, followerCount, latestFollower, subCount, latestSub);
+                fetchApi(channelId, api);
             }, 60000); // 60 seconds
+
+            const labelDescription = document.getElementById('label-description');
+            const labelValue = document.getElementById('label-value');
+
+            setInterval(function() {
+                if(fetched) {
+                    const label = document.getElementById('label');
+
+                    const temp = Object.entries(data);
+                    labelDescription.innerText = temp[i][0];
+                    labelValue.innerText = temp[i][1];
+
+                    label.classList.add('visible');
+                    setTimeout(function() {
+                        label.classList.remove('visible');
+                        i++;
+                        if(i > 3) {
+                            i = 0;
+                        }
+                    }, 10000);
+                }
+            }, 60000);
         }
     }
 })();
 
-function fetchApi(channelId, api, followerCount, latestFollower, subCount, latestSub) {
+function fetchApi(channelId, api) {
     // Get channel follows
     api.get(`users/follows?to_id=${channelId}`).then(response => {
-        followerCount.innerText  = response.total;
-        latestFollower.innerText = response.data[0].fromName;
+        data["Follower"]  = response.total;
+        data["Letzter Follower"] = response.data[0].fromName;
         
         api.get(`channels/${channelId}/subscriptions?direction=desc&limit=1`, { version: 'kraken' }).then(response => {
-            subCount.innerText  = response.total;
-            latestSub.innerText = response.subscriptions[0].user.displayName;
+            data["Subscriber"] = response.total;
+            data["Letzter Sub"] = response.subscriptions[0].user.displayName;
+
+            fetched = true;
         });
     });
 }
